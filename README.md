a8baa9b9a0274ee4117bf60d57da3c87c7c86f09
Prince Rana <ranaprince45may@gmail.com>
2025-01-30 23:31:37 +0530

    updated  the readme file

--- README.md
+++ README.md
@@ -4,4 +4,5 @@
 -register and login than user redirect to /Shop page
 
 ## AdminRouter (Owners) 
--get /owners/admin => here the Admin where admin create the pproducts and 
+-get /owners/admin => here the Admin where admin create the products and user can show all the 
+product on the /shop route
--- app.js
+++ app.js
@@ -5,7 +5,6 @@ const path = require("path");
 const expressSession = require("express-session");
 const flash = require("connect-flash");
 
-
 const ownersRouter = require("./routes/ownersRouter");
 const productsRouter = require("./routes/productsRouter");
 const usersRouter = require("./routes/usersRouter");
